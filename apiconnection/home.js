90767c47e112d65d5655676406f0c6397b37d51d
avinashsinghavin <avinashsinghavin@gmail.com>
2020-09-11 21:28:15 +0530

    Adding Brand Bottom and Brand section Slider
    Changing Bottom's Product Brand.
    Adding Slider View of Brand At Top.

--- apiconnection/home.js
+++ apiconnection/home.js
@@ -12,16 +12,25 @@ async function popular_brand() {
 
 async function brand(data) {
     var data_json = JSON.parse(data);
-    var branddiv = document.getElementById("Popular_brand_div").innerHTML;
+    var bannercount = '<div class="carousel-item active"><img class="d-block w-100" src="'+data_json.response[0].brand_img+'" style="height: 350px;"><a class="carousel-control-prev" style="width: 100%;"></a></div>'; 
+    var BannerImage = '<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active" style="background-color: red;"></li>';
     var k = "<div class='container'><div class='row'>";
-    var banner = "";
     console.log(data_json.response[0].brand_name);
     console.log(data_json);
 
     for(var i = 0; i < data_json.response.length; i++) {
-        k += "<div class='col-sm img-fluid' style='padding 5px; margin:5px;> <a href='#'><img src='"+data_json.response[i].brand_img+"'alt='"+data_json.response[i].brand_name+"' style='margin: auto;'/></a> </div>";
-        // banner += '<div class="item"><a href="#"><img src="images/01.jpg" alt="Main Banner" class="img-responsive" /></a></div>';
+       console.log(i);
+       k += "<div class='border border-danger rounded col-sm img-fluid' style='padding: 5px; margin:5px;'> <a href='#'><img src='"+data_json.response[i].brand_img+"'alt='"+data_json.response[i].brand_name+"' style='margin: auto;'/></a> </div>";
+       if((i+1) % 5 == 0){
+           k += "</div></div><div class='container' style><div class='row'>";
+       }
+        if(i != 0) {
+            BannerImage += '<li data-target="#carouselExampleIndicators" data-slide-to="'+(i)+'"style="background-color: red;"></li>';
+            bannercount+= '<div class="carousel-item "><img class="d-block w-100" src="'+data_json.response[i].brand_img+'" style="height: 350px;"><a class="carousel-control-prev" style="width: 45%;"></a></div>';
+        }
     }
-    var branddiv = document.getElementById("Popular_brand_div").innerHTML = k + '</div></div>';
-    // var Bannerdiv = document.getElementById('bannersection').innerHTML = banner;
+   
+    document.getElementById("Popular_brand_div").innerHTML = k + '</div></div>';
+    document.getElementById('BannerImage').innerHTML = bannercount;
+    document.getElementById('BannerCount').innerHTML = BannerImage;
 }
