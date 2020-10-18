9534031faf8138e16ba56a0284b79b749808e720
avinashsinghavin <avinashsinghavin@gmail.com>
2020-10-18 08:01:56 +0530

    India as Default in login
    Vendor In center

--- apiconnection/login.js
+++ apiconnection/login.js
@@ -31,14 +31,15 @@ window.onload = function() {
                 signInFlow: 'popup',
                 signInSuccessUrl: '',
                 signInOptions: [
-                  // Leave the lines as is for the providers you want to offer your users.
-                  //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                  //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                  //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-                  //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-                  //firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                  firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-                //   defaultCountry:'IN'
+                  {
+                    provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+                    recaptchaParameters: {
+                      type: 'image', // 'audio'
+                      size: 'normal', // 'invisible' or 'compact'
+                      badge: 'bottomleft' //' bottomright' or 'inline' applies to invisible.
+                    },
+                    defaultCountry: 'IN'
+                  }
                 ],
                 // Terms of service url.
                 tosUrl: 'login.html',
